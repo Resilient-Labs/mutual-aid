2517bb4bd670af3ef6dff08bc0fb22ddd516dd4c
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2022-01-06 12:21:51 -0500

    first draft of dashboard and settings pages

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import Landing from './Landing';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Settings from './components/Settings';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
@@ -15,6 +18,8 @@ ReactDOM.render(
       <Route path="/landing" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
