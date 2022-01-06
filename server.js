334a8c5303f548ac28054a6ab1934759b6398ffa
martecodes <jeanmarte93@gmail.com>
2022-01-06 13:03:01 -0100

    first push from the backend team. working how to connect with frontend codes

---
+++ server.js
@@ -0,0 +1,56 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+const express  = require('express');
+const multer   = require('multer');
+const app      = express();
+const port     = process.env.PORT || 8080;
+const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash    = require('connect-flash');
+
+const morgan       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const session      = require('express-session');
+const ObjectId     = require('mongodb').ObjectId
+
+const configDB  = require('./config/database.js');
+const dotenv    = require('dotenv').config()
+
+
+// configuration ===============================================================
+mongoose.connect(configDB.url, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  require('./app/routes.js')(app, passport, db);
+}); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // used to access files under the public folder
+
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'rcbootcamp2021b', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+// launch ======================================================================
+app.listen(port);
+console.log('Mutual Aid Bank is connected to port ' + port);
