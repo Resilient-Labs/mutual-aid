9680158b1b7d3405fd714b30055b11fdf5aa62ef
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2022-01-06 11:37:14 -0500

    added routing and organized files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Landing from './Landing';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Landing />
+      </>
+    );
+  }
+}
+
+export default App;
