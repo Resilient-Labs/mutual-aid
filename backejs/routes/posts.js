e538cf1b1adc5264d773dd080d87fbf66d26b955
NDNey <ndavidney@gmail.com>
2022-01-07 14:59:19 -0500

    MVC Refactored added to backejs

---
+++ backejs/routes/posts.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+
+router.post("/createPost", upload.single("file"), postsController.createPost);
+
+module.exports = router;
